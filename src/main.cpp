0b959186f20d6ef2229257c273d88a931806ee76
jstri114 <jstri114@students.kennesaw.edu>
2023-08-02 01:18:09 -0400

    initial

--- src/main.cpp
+++ src/main.cpp
@@ -2,21 +2,27 @@
 
 #include <CAN.h>
 
-SPIClass.setMOSI(11);
-	//void setMISO(uint8_t pin);
-	//void setSCK(uint8_t pin);
+// SPIClass.setMOSI(11);
+// 	//void setMISO(uint8_t pin);
+// 	//void setSCK(uint8_t pin);
+
 
 
 void setup() {
+  CAN.setPins(10);
+
   Serial.begin(9600);
-  while (!Serial);
-int pin = MCP2515Class::_csPin;
-  Serial.println("CAN Sender");
-  Serial.print(pin);
+  // while (!Serial);
+// int pin = MCP2515Class::_csPin;
+//   Serial.println("CAN Sender");
+//   Serial.print(pin);
   // start the CAN bus at 500 kbps
   if (!CAN.begin(500E3)) {
     Serial.println("Starting CAN failed!");
     while (1);
+  }
+  else {
+    Serial.println("CAN started successfully!");
   }
 }
 
@@ -36,18 +42,18 @@ void loop() {
 
   delay(1000);
 
-  // send extended packet: id is 29 bits, packet can contain up to 8 bytes of data
-  Serial.print("Sending extended packet ... ");
+  // // send extended packet: id is 29 bits, packet can contain up to 8 bytes of data
+  // Serial.print("Sending extended packet ... ");
 
-  CAN.beginExtendedPacket(0xabcdef);
-  CAN.write('w');
-  CAN.write('o');
-  CAN.write('r');
-  CAN.write('l');
-  CAN.write('d');
-  CAN.endPacket();
+  // CAN.beginExtendedPacket(0xabcdef);
+  // CAN.write('w');
+  // CAN.write('o');
+  // CAN.write('r');
+  // CAN.write('l');
+  // CAN.write('d');
+  // CAN.endPacket();
 
-  Serial.println("done");
+  // Serial.println("done");
 
-  delay(1000);
+  // delay(1000);
 }
